b7460290bad58f1b81d352dfadf8941ae3054419
Alexey-beercode <alsemkovbn@gmail.com>
2024-05-21 00:46:52 +0300

    Added view

---
+++ OnlineBank/OnlineBank.Data/ViewModels/AccountsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using OnlineBank.Data.Entity;
+
+namespace OnlineBank.Data.ViewModels;
+
+public class AccountsViewModel
+{
+    public List<Account> Accounts { get; set; }
+    public decimal Balance { get; set; }
+}
